b8b23e8e9fb01c514f3ddd941c94bdd00a3d462a
Marco Malaspina <marcomalaspina@MacBook-Pro-de-Marco.local>
2020-03-21 13:25:54 -0500

    Added logic

--- js/main.js
+++ js/main.js
@@ -1,31 +1,123 @@
 
-
-let inputGeoloc = document.getElementById('inputGeoloc');
+// Geoloc
+let inputGeolocCiudad = document.getElementById('inputGeolocCiudad');
+let inputGeolocDepartamento = document.getElementById('inputGeolocDepartamento');
 let btnGeoloc = document.getElementById('btnGeoloc');
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&key=AIzaSyA9VCeYTBLZprpSfqWOtlw-j9rQUVTsCzg", function (data) {
-                adresse = data.results[7].formatted_address
-                sessionStorage.setItem('geoloc', adresse)
-                var data = sessionStorage.getItem('geoloc');
-                inputGeoloc.value = data
+                let dataGeoloc = [];
+
+                adresseVille = data.results[7].formatted_address
+                adresseProvince = data.results[9].formatted_address
+
+                splitVille = adresseVille.split(", ");
+                ville = splitVille[0];
+                splitProvince = adresseProvince.split(", ");
+                province = splitProvince[0];
+
+                dataGeoloc.push(ville);
+                dataGeoloc.push(province);
+
+                // sessionStorage.setItem('geoloc', dataGeoloc)
+                // var data = sessionStorage.getItem('geoloc');
+
+                inputGeolocCiudad.value = dataGeoloc[0];
+                inputGeolocDepartamento.value = dataGeoloc[1];
+
             })
         })
     }
 })
 
-btnGeoloc.addEventListener("click", () => {
+btnGeoloc.addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&key=AIzaSyA9VCeYTBLZprpSfqWOtlw-j9rQUVTsCzg", function (data) {
-                adresse = data.results[7].formatted_address
-                sessionStorage.setItem('geoloc', adresse)
-                var data = sessionStorage.getItem('geoloc');
-                inputGeoloc.value = data
+                let dataGeoloc = [];
+
+                adresseVille = data.results[7].formatted_address
+                adresseProvince = data.results[9].formatted_address
+
+                splitVille = adresseVille.split(", ");
+                ville = splitVille[0];
+                splitProvince = adresseProvince.split(", ");
+                province = splitProvince[0];
+
+                dataGeoloc.push(ville);
+                dataGeoloc.push(province);
+
+                // sessionStorage.setItem('geoloc', dataGeoloc)
+                // var data = sessionStorage.getItem('geoloc');
+
+                inputGeolocCiudad.value = dataGeoloc[0];
+                inputGeolocDepartamento.value = dataGeoloc[1];
             })
         })
     }
 })
+// Form Logic
+let inputFebre = document.getElementById('inputFebre');
+let infoFebre = document.getElementById('infoFebre');
+let infoTos = document.getElementById('infoTos');
 
+
+function changeState(id) {
+    switch (id) {
+        case inputFebre:
+            if (id.value === 'febre') {
+                infoFebre.removeAttribute('disabled');
+            } else {
+                infoFebre.setAttribute('disabled', '')
+            }
+            break;
+
+        case inputTos:
+            if (id.value === 'tos') {
+                infoTos.removeAttribute('disabled');
+            } else {
+                infoTos.setAttribute('disabled', '')
+            }
+            break;
+
+        case inputMoqueo:
+            if (id.value === 'moqueo') {
+                infoMoqueo.removeAttribute('disabled');
+            } else {
+                infoMoqueo.setAttribute('disabled', '')
+            }
+            break;
+
+        case contactoSi:
+            if (id.value === 'si') {
+                infoContacto.removeAttribute('disabled');
+            }
+            break;
+
+        case contactoNo:
+            if (id.value === 'no') {
+                infoContacto.setAttribute('disabled', '')
+            }
+            break;
+
+        case numeroSi:
+            if (id.value === 'si') {
+                inputTel.removeAttribute('disabled');
+            }
+            break;
+
+        case numeroNo:
+            if (id.value === 'no') {
+                inputTel.setAttribute('disabled', '')
+            }
+            break;
+
+
+
+    }
+
+
+
+}
