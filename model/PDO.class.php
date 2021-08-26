2a8202a58a8200d12aa3ed394bf151f954ed6b04
Marc-Antoine Malaspina <51450478+Marco8301@users.noreply.github.com>
2021-08-26 03:02:16 -0500

    Update PDO.class.php

--- model/PDO.class.php
+++ model/PDO.class.php
@@ -2,8 +2,6 @@
 
 try {
     $db = new PDO ("mysql:host=localhost; dbname=covid; charset=utf8", "root", "root");
-    // $db = new PDO ("mysql:host=covid19-prd.c3yyzpjxs5ol.us-east-2.rds.amazonaws.com; dbname=covid; charset=utf8", "master", "zaq1xsw2cde3");
-    // $db = new PDO ("mysql:host=covid.c3yyzpjxs5ol.us-east-2.rds.amazonaws.com; port=3306; dbname=covid; charset=utf8", "master", "zaq1xsw2cde3");
 }
 
 catch (PDOException $e) {
